91841e97352052d01bf866ad05215f4104f92c50
Amaury Franssen <amauryfranssen@gmail.com>
2024-08-02 20:10:51 +0200

    fix(intl-navigation): fix import name -3

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,14 +3,14 @@ import {
   createLocalizedPathnamesNavigation,
   Pathnames
 } from 'next-intl/navigation';
-//import {Pathnames, LocalePrefix} from 'next-intl/routing';
+import { LocalePrefix} from 'next-intl/routing';
 
 export const defaultLocale = 'en' as const;
 export const locales = ['en', 'fr'] as const;
 
 export const localePrefix: LocalePrefix<typeof locales> = 'always';
 
-export const pathnames: Pathnames<typeof locales> = {
+export const pathnames = {
   '/': '/',
   '/about': {
     en: '/about',
