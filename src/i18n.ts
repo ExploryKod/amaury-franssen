03f333142236d2b52025dce16bd3fd6e5a0ee38d
Amaury Franssen <amauryfranssen@gmail.com>
2024-08-03 02:04:38 +0200

    suppress favicon before searching for a better one

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import { getRequestConfig } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 
-export const locales = ['en', 'fr', 'es']
+export const locales = ['fr', 'en', 'es']
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound()
